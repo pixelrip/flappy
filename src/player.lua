ffb3e170c83357a415dc1b4f2972766906ff7a6b
pixelrip <go@pixel.rip>
2025-09-28 12:42:48 -0500

    Add flap mechanic to player update logic
    Introduces FLAP_STRENGTH constant and modifies the player's update function to handle flap input and vertical velocity adjustment. This enables a new movement mechanic triggered by button press.

--- src/player.lua
+++ src/player.lua
@@ -1,4 +1,6 @@
 player = {
+    -- constants
+    FLAP_STRENGTH = -3.5,
     -- pos
     x = 10,
     y = 10,
@@ -11,6 +13,15 @@ player = {
     vy = 0,
 
     update = function(self)
+        if btnp(4) then
+            if self.vy < 0 then return end
+            self.vy += self.FLAP_STRENGTH
+        end
+
+        if self.vy < 0 then 
+            self.vy += 0.1
+        end
+
         gs:apply(self)
     end,
 
